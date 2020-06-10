ad743d80f1ecb235dbeb261699f41e3a6381bd1f
GentlyJian <1183048003@qq.com>
2020-06-10 08:04:42 +0800

    vue后台管理

--- src/permission.js
+++ src/permission.js
@@ -12,34 +12,42 @@ const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
   // start progress bar
+  // debugger
   NProgress.start()
 
   // set page title
   document.title = getPageTitle(to.meta.title)
 
   // determine whether the user has logged in
+  // 判断是否有token
   const hasToken = getToken()
 
   if (hasToken) {
+    // 如果有token,访问login界面会跳转到 / 根目录下
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
       next({ path: '/' })
       NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
+      // 如果访问的是其他路径， 则通过在vuex里获取角色信息
       // determine whether the user has obtained his permission roles through getInfo
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
       if (hasRoles) {
         next()
       } else {
+        // 如果vuex里没有角色信息，则调用接口获取角色信息
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          // 调用接口设置用户的基本信息
           const { roles } = await store.dispatch('user/getInfo')
 
           // generate accessible routes map based on roles
+          // 通过角色获取对应的路由
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
           // dynamically add accessible routes
+          // 动态添加路由
           router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
@@ -56,11 +64,12 @@ router.beforeEach(async(to, from, next) => {
     }
   } else {
     /* has no token*/
-
+    // 如果在白名单里则直接访问
     if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
       next()
     } else {
+      // 否则跳转到登录界面
       // other pages that do not have permission to access are redirected to the login page.
       next(`/login?redirect=${to.path}`)
       NProgress.done()
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -6,6 +6,8 @@ import { asyncRoutes, constantRoutes } from '@/router'
  * @param route
  */
 function hasPermission(roles, route) {
+  // 如果该路由没有设置meta属性，则直接返回为true
+  // 否则通过meta的 roles判断是否包含该 roles
   if (route.meta && route.meta.roles) {
     return roles.some(role => route.meta.roles.includes(role))
   } else {
@@ -15,6 +17,7 @@ function hasPermission(roles, route) {
 
 /**
  * Filter asynchronous routing tables by recursion
+ * 过滤对应角色的动态路由
  * @param routes asyncRoutes
  * @param roles
  */
@@ -27,6 +30,7 @@ export function filterAsyncRoutes(routes, roles) {
       if (tmp.children) {
         tmp.children = filterAsyncRoutes(tmp.children, roles)
       }
+      // 将过滤的动态路由返回
       res.push(tmp)
     }
   })
@@ -41,7 +45,9 @@ const state = {
 
 const mutations = {
   SET_ROUTES: (state, routes) => {
+    // 将动态路由放入状态 addRoutes
     state.addRoutes = routes
+    // 将动态路由push进 静态路由合成所有可访问的卤藕
     state.routes = constantRoutes.concat(routes)
   }
 }
@@ -50,11 +56,14 @@ const actions = {
   generateRoutes({ commit }, roles) {
     return new Promise(resolve => {
       let accessedRoutes
+      // 如果权限包含 admin 则 获取所有动态路由
       if (roles.includes('admin')) {
         accessedRoutes = asyncRoutes || []
       } else {
+        // 否则过滤 获取对应角色的路由
         accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
       }
+      // 设置路由
       commit('SET_ROUTES', accessedRoutes)
       resolve(accessedRoutes)
     })
